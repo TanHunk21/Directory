78dd4259edf372ffb9950f5d650805e75f1ebba3
Opilane TTHK <opilane@tthk.ee>
2020-11-12 13:26:50 +0200

    Directory

---
+++ DirectoriesAndFiles/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoriesAndFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string rootPath = @" C:\Users\opilane\samples";
+            // get te full path of all the directories at the rootpath
+            string[] allDirectories = Directory.GetDirectories(rootPath);
+            //ReadArray(allDirectories);
+
+            // get nested directories
+            string[] includeNestedDirectories = Directory.GetDirectories(rootPath, "*",SearchOption.AllDirectories);
+            ReadArray(includeNestedDirectories);
+
+
+            //get directory files
+            string[] files = Directory.GetFiles(rootPath, "*.*",SearchOption.AllDirectories);
+            ReadArray(files);
+
+
+
+        }
+
+        public static void ReadArray(string[] array)
+        {
+            foreach(string line in array)
+            {
+                Console.WriteLine(line);
+
+            }
+        }
+        
+    }
+}
---
+++ WriteFilesAndDirectories/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace WriteFilesAndDirectories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WriteFilePath();
+
+        }
+        public static void WriteFilePath()
+        {
+            Console.WriteLine("Tere Hommmikust");
+
+            string filesPath = @"C:\Users\opilane\Samples\files.txt";
+            string Directories = @"C:\Users\opilane\Samples";
+
+            string[] files = Directory.GetFiles(Directories, "*.*", SearchOption.AllDirectories);
+
+            //save to file
+            File.WriteAllLines(filesPath, files);
+
+
+
+        }
+    }
+}
